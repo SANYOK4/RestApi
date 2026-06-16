b1f7516d306f71471258bd05ab248a2770542ff0
Oleksandr <belinskicsasa@gmail.com>
2026-06-16 19:13:44 +0300

    Fix DATABASE_URL from env variable

--- LabApi/models/database.py
+++ LabApi/models/database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-# Не забудь змінити 'password' на свій!
-DATABASE_URL = "postgresql+asyncpg://postgres:WeRtY54321@localhost:54321/library_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://postgres:WeRtY54321@localhost:54321/library_db"
+)
 
 engine = create_async_engine(DATABASE_URL)
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
